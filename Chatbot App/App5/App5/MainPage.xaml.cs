42f5e50d2ed14b04c17c95e63fd917a0ad551cea
Raghav <raghavgarg32@gmail.com>
2018-09-09 16:45:40 +1200

    Committing app

---
+++ Chatbot App/App5/App5/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            var browser = new WebView();
+            browser.Source = "https://webchat.botframework.com/embed/contosobankbot32?s=xV_GXml_rw0.cwA.Fbc.24iEuHTtTJo2Hvs21Bn7N5ZLk-lKN5EBTg8QGliQywc";
+            this.Content = browser;
+        }
+	}
+}
